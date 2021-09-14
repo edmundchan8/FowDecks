cb1c727002d8c833e27299721627ac494c0e0677
edmundchan8 <pokedev8@gmail.com>
2021-09-14 00:15:33 -0700

    2nd attemp to commit changes to git

--- Controllers/CardDatabaseController.cs
+++ Controllers/CardDatabaseController.cs
@@ -6,16 +6,22 @@ using System.Threading.Tasks;
 using FowDecks.Models;
 using FowDecks.ViewModels;
 using FowDecks.Data;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
 
 namespace FowDecks.Controllers
 {
     public class CardDatabaseController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public CardDatabaseController(ApplicationDbContext db)
+        private readonly IWebHostEnvironment webHostEnvironment;
+        public CardDatabaseController(ApplicationDbContext db, IWebHostEnvironment HostEnv)
         {
             _db = db;
+            webHostEnvironment = HostEnv;
         }
+        public int MyProperty { get; set; }
+
         public IActionResult Index()
         {
             CardDatabaseViewModel cardDbViewModel = new()
@@ -31,20 +37,61 @@ namespace FowDecks.Controllers
         {
             return View();
         }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Card card)
+        public IActionResult Create(CreateCardViewModel createCardVM)
         {
-            if (ModelState.IsValid)
+            try 
             {
-                _db.Add(card);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    string uniqueFileName = null;
+                    if (createCardVM.Image != null)
+                    {
+                        string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                        uniqueFileName = Guid.NewGuid().ToString() + "_" + createCardVM.Image.FileName;
+                        string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                        createCardVM.Image.CopyTo(new FileStream(filePath, FileMode.Create));
+                    }
+
+                    int AtkValue = 0;
+
+                    Card card = new() 
+                    {
+                        Name = createCardVM.Name,
+                        Image = uniqueFileName,
+                        Type = createCardVM.Type,
+                        Cost = createCardVM.Cost,
+                        TotalCost = createCardVM.TotalCost,
+                        Atk = AtkValue,
+                        Def = createCardVM.Def,
+                        Divinity = createCardVM.Divinity,
+                        Race = createCardVM.Race,
+                        Attribute = createCardVM.Attribute,
+                        CardText = createCardVM.CardText,
+                        FlavorText = createCardVM.FlavorText,
+                        CardNumber = createCardVM.CardNumber,
+                        Rarity = createCardVM.Rarity,
+                        Artist = createCardVM.Artist,
+                        Set = createCardVM.Set,
+                        Format = createCardVM.Format
+                    };
+                    _db.Add(card);
+                    _db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(createCardVM);
+                }
             }
-            else 
+
+            catch (Exception e)
             {
-                return View(card);
+                Console.WriteLine(e);
             }
+            return RedirectToAction("Index");
         }
     }
 }
