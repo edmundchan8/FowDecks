14807890214efb1d266b1c3ef08d18496b8e1a0c
edmundchan8 <pokedev8@gmail.com>
2021-09-18 00:57:40 -0700

    changing variable back to cardText

--- Controllers/CardDatabaseController.cs
+++ Controllers/CardDatabaseController.cs
@@ -45,7 +45,7 @@ namespace FowDecks.Controllers
 
                 var cardName = "";
                 var cardType = "";
-                var text = "";
+                var cardText = "";
 
                 if (!String.IsNullOrEmpty(cardDBVM.Card.Name))
                     cardName = cardDBVM.Card.Name;
@@ -54,10 +54,10 @@ namespace FowDecks.Controllers
                     cardType = cardDBVM.Card.Type;
 
                 if(!String.IsNullOrEmpty(cardDBVM.Card.CardText))
-                    text = cardDBVM.Card.CardText;
+                    cardText = cardDBVM.Card.CardText;
 
 
-                cardDBVM.Cards = cards.Where(c => c.CardText.Contains(text)
+                cardDBVM.Cards = cards.Where(c => c.CardText.Contains(cardText)
                     && c.Name.Contains(cardName)
                     && c.Type.Contains(cardType));
                 
