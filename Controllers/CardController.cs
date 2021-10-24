f8afc2a62641b4d31f528f5c7c48c2042c80679c
edmundchan8 <pokedev8@gmail.com>
2021-10-23 22:49:48 -0700

    Clicking on a card takes you to a view with details of the card

---
+++ Controllers/CardController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FowDecks.Models;
+using FowDecks.Data;
+
+namespace FowDecks.Controllers
+{
+    public class CardController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CardController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index(int id)
+        {
+            Card card = _db.Cards.Where(c => c.Id == id)
+                .FirstOrDefault();
+
+            return View(card);
+        }
+    }
+}
